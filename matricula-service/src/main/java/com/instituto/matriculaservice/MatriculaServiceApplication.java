395461fcb2aaa0c43cc396a6a1acf8a5bfa1b1b8
felyp244 <felipe.yan12@gmail.com>
2026-06-26 19:57:06 -0500

    Matricula agregada, falta desarrollarla

---
+++ matricula-service/src/main/java/com/instituto/matriculaservice/MatriculaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.instituto.matriculaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaServiceApplication.class, args);
+	}
+
+}
